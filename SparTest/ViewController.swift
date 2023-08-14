0df18d1ce61782f407b43518d118319259af4fa8
Vadim <vadim_kimm@icloud.com>
2023-08-14 14:01:27 +0300

    Initial Commit

---
+++ SparTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparTest
+//
+//  Created by Vadim Kim on 14.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
